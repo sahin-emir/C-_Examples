d206cbb00fa361016bd546e6c5b936400d5a2eed
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-04-13 16:16:01 +0300

    Update AlgoritmaÖrnekler.cs

--- AlgoritmaÖrnekler.cs
+++ AlgoritmaÖrnekler.cs
@@ -55,5 +55,31 @@
                 gir=Convert.ToInt32(Console.ReadLine());
                 asal[j]=gir;
             }
-    
+    string[] isimler = new string[5];//Bineary-Search metodusuz çözümü 
+            string anahtar;
+            isimler[0] = "Ahmet";
+            isimler[1] = "Alş";
+            isimler[2] = "Furkan";
+            isimler.SetValue( "Halil" ,3 );
+            isimler.SetValue ( "Kadir",4 );
+            Array.Sort(isimler);
+
+
+            Console.WriteLine("aranacak kelime giriniz");
+            anahtar = Console.ReadLine();
+            
+            int i=-1;
+            while (true)
+            {
+                i++;
+                if(isimler[i] == anahtar){
+                    Console.WriteLine("aradaıgınız karakter vardır index no :"+i);
+                    break;
+                }
+                else{
+                    Console.WriteLine("aradıgını değer yoktur ");
+                }
+            }
+           
+            Console.ReadLine();
     
