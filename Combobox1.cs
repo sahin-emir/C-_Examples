008456cc0dd0b0b895bf2cd2e440816c2740889f
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-06-05 16:23:15 +0300

    Create Combobox1.cs

---
+++ Combobox1.cs
@@ -0,0 +1,22 @@
+// combobox veri karşılaştırma
+
+
+comboBox1.Items.Add(textBox1.Text);
+
+if (comboBox1.Items.IndexOf(textBox1.Text) != -1) {
+
+//MessageBox.Show("Kayıtlı Bilgi Vardır.");
+    DialogResult secenek= MessageBox.Show("İşlem yapılsın mı?", "Dikkat",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+ 
+              if (secenek == DialogResult.Yes)
+              {
+                  MessageBox.Show("Ekleme yapabilirsiniz.");
+              }
+              else if(secenek==DialogResult.No)
+              {
+                 this.Close();
+                  Application.Exit();
+              }
+ }
+else
+comboBox1.Items.Add(textBox1.Text);
