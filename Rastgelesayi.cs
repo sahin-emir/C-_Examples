e5f6c286df19f242a14e694fb8fde4fc115a9fd3
sahin-emir <93335449+sahin-emir@users.noreply.github.com>
2022-06-05 16:44:38 +0300

    Update Rastgelesayi.cs

--- Rastgelesayi.cs
+++ Rastgelesayi.cs
@@ -1,9 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+
 class HelloWorld {
   static void Main() {
    
     int[] sayi = new int[20]; // 20 elemanlı dizi oluşturuldu.
     Random rnd = new Random();
     int enbuyuk=0;
+    int ort=0;
+    int toplam=0;
+    
  
    for (int i =0;i<sayi.Length;i++){
        sayi[i]=rnd.Next(100,999);
@@ -22,6 +35,12 @@ class HelloWorld {
        Console.WriteLine(eleman);
        
    }
+   // Ortalama
+   foreach(int ortat in sayi){
+       toplam+=ortat;
+       ort=toplam/20;
+   }
+   Console.WriteLine("ortlalamnız"+ort);
    
    Console.WriteLine("en bhuyuk"+enbuyuk);
    Console.ReadKey();
